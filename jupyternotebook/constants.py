0c3745d0e419cce568e3daa1b4175a6708bd631f
SanthoshMLengineer <santhoshkumarks922@gmail.com>
2023-11-13 15:23:21 +0530

    Model Training files

---
+++ jupyternotebook/constants.py
@@ -0,0 +1,21 @@
+"""
+This file helps to lebeling 
+"""
+dict_expression_num = {"angry": 0,
+					   "disgust":1,
+					   "fear":2,
+					   "happy":3,
+					   "neutral":4,
+					   "sad":5,
+					   "surprise":6
+					}
+
+
+dict_num_expression = {"0":"angry",
+					   "1":"disgust",
+					   "2":"fear",
+					   "3":"happy",
+					   "4":"neutral",
+					   "5":"sad",
+					   "6":"surprise"
+					}
